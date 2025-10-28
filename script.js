f458171d8bbc58ceb3d5938a88874a0b1c440300
Onetatcode <alikhanclues.@gmail.com>
2025-10-29 00:18:30 +0500

    Progress

--- script.js
+++ script.js
@@ -91,6 +91,19 @@ function page3VideoAnimation() {
     })
 }
 
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity=1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity=0
+        elem.childNodes[3].load()
+    })
+})
+
 
 //page3VideoAnimation()    
 //navAnimation()
